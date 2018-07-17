a50c1c243f9a2b963d8fd7ea781911103002139b
Jannaee Sick <jannaee@gmail.com>
2018-07-17 02:01:27 -0400

    refactor:Rebrand of site 2018 ver 1.0

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# jannaee.github.io
-Jannaee's personal website, a place to showcase professional work as well as the ongoings in my day to day life. 
+Setting up local environment
+****************************
+Previewing from a python server, be sure to have at least 3.6.5 installed from the "Windows x86-64 executable installer" version.
 
-The version of this website as you see it today was originally created with Google Web Starter Kit "A front-end template that helps you build fast, modern mobile web apps."
 
-As the year progresses I have plans to redesign this website from scratch.
+Test for python with python -V
 
-Goals include:
-Experimenting with GSAP and svg animations.
-Implementing ARIA Roles, responsive images using srcset.
-Incorporating a task runner to help automate task flows on build.
-Displaying more JavaScript projects.
+http://localhost:49964/
+
+
---
+++ js/main.js
@@ -0,0 +1,68 @@
+// Code for Parallax scrolling effects 
+//attribute: https://1stwebdesigner.com/parallax-scrolling-tutorial/
+'use strict';
+
+$(document).ready(function () {
+    $("a[href*='#']").each(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            && location.hostname == this.hostname
+            && this.hash.replace(/#/, '')) {
+            let $targetId = $(this.hash), 
+                $targetAnchor = $('[name=' + this.hash.slice(1) + ']');
+            let $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+            if ($target) {
+                let targetOffset = $target.offset().top;
+                $(this).click(function () {
+                    $("#nav a").removeClass("active");
+                    $(this).addClass('active');
+                    $('html, body').animate({ scrollTop: targetOffset }, 1000);
+                    return false;
+                });
+            }
+        }
+    });
+});
+
+//https://codemyui.com/hide-header-navigation-on-scroll-down-and-show-on-scroll-up/
+$(document).ready(function () {
+    let c, 
+        currentScrollTop = 0,
+        navbar = $('nav');
+    $(window).scroll(function () {
+        let a = $(window).scrollTop();
+        let b = navbar.height();
+        currentScrollTop = a;
+        if (c < currentScrollTop && a > b + b) {
+            navbar.addClass("scrollUp");
+        } else if (c > currentScrollTop && !(a <= b)) {
+            navbar.removeClass("scrollUp");
+        }
+        c = currentScrollTop;
+    });
+});
+
+//Hamburger icon for small screens with an animated feature
+function animatedMenu(menuClick){
+    menuClick.classList.toggle("change");
+    let x = document.getElementById('myHamNav');
+    if (x.className === "nav-container") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-container";
+    }
+}
+
+// Registering Serviceworker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('sw.js', {scope: '/'})
+    .then(function(event) {
+      console.log("[Step 1. Service Worker from main.js] Registered", event.scope);
+      })
+    .catch(function(error){
+      console.log('[Step 1. Service Worker from main.js] Error on Registration', error);
+      }
+    )
+  }
+
+
